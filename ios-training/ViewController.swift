c8fa1b395453a5401e36fcce35c2fdc1657765dd
Minoru Nakamura <nakamura200701@gmail.com>
2021-04-03 23:46:45 +0900

    first commit

---
+++ ios-training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-training
+//
+//  Created by 中村実 on 2021/04/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
